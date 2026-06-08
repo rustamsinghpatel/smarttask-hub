f0c815e33da278b605418bc399ee35f9851ad3ca
Rustam Patel <53313031+rustamsinghpatel@users.noreply.github.com>
2026-06-08 19:15:47 +0530

    start again  backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
-
-// Connect Database
-connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +16,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
